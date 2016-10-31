80ce0dc3d0ad483ca5fbe93f3c76a202b9a5bd5a
DexterZhao <dexter.zhza@gmail.com>
2016-10-31 15:13:57 -0700

    v1.0

--- v1/src/main/java/twitter_craw_maven/v1/App.java
+++ v1/src/main/java/twitter_craw_maven/v1/App.java
@@ -1,13 +1,34 @@
 package twitter_craw_maven.v1;
 
+import java.util.List;
+
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws TwitterException
     {
+    	ConfigurationBuilder confBuilder = new twitter4j.conf.ConfigurationBuilder();
+    	
+    	confBuilder.setOAuthConsumerKey("1oZKJhXaEV6XKOrgeRk3jkGVj");
+    	confBuilder.setOAuthConsumerSecret("zhMFqjDZnyGHKFT8gNjw6HfG1zpQ2JdsIIggA2k8ka8c9XicOo");
+    	confBuilder.setOAuthAccessToken("780871500903559169-aDLQ12mUVopKB7S06XhNsNwudCgd2ky");
+    	confBuilder.setOAuthAccessTokenSecret("sAULr1rfDJeey1jfuCVzl2ssunQ8bCyRx4xnjln0Jsb5z");
+    	//Configuration config = confBuilder.build();
+    	
+    	TwitterFactory tf = new TwitterFactory(confBuilder.build());
+    	twitter4j.Twitter twitter= tf.getInstance();
+    	
+    	List<status> status =twitter.getHomeTimeline();
+
+    	
+        
         System.out.println( "Git try" );
     }
 }
